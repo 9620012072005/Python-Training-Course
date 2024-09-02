1e346953a27dbb4b716b2105a90cbe537756da95
9620012072005 <chellammal786@gmail.com>
2024-09-03 00:24:33 +0530

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Question(models.Model):
-    text = models.CharField(max_length=255)
-
-class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    text = models.CharField(max_length=255)
-    is_correct = models.BooleanField(default=False)
